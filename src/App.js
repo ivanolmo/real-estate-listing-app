a79e7e38dc0c9e1c23eb5cf3fb1d8bdb05d590d7
Ivan Olmo <ivan.olmo@gmail.com>
2022-01-17 11:03:50 -0600

    Add ForgotPassword route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Explore from './pages/Explore';
 import Offers from './pages/Offers';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path='/profile' element={<SignIn />} />
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
         <Navbar />
       </Router>
