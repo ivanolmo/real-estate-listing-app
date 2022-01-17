a1c122fbd4e5a794645809c09161e07ef972946a
Ivan Olmo <ivan.olmo@gmail.com>
2022-01-16 19:35:01 -0600

    Add main routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+
 function App() {
-  return <>Working</>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
