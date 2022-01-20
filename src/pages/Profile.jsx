e26761e1a296afb17cc9691b4788a210a1704735
Ivan Olmo <ivan.olmo@gmail.com>
2022-01-20 09:37:13 -0600

    Refactor profile page with user state and logout button

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getAuth } from 'firebase/auth';
+
 function Profile() {
-  return <div className='page__header'>Profile</div>;
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate('/');
+  };
+
+  return (
+    <div className='profile'>
+      <header className='profile__header'>
+        <p className='page__header'>My Profile</p>
+        <button type='button' className='logout__btn' onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default Profile;
