0e2d65f7dc3d74e61d7e7a1d015e0408988151af
Ivan Olmo <ivan.olmo@gmail.com>
2022-01-22 15:35:36 -0600

    Fix unmounted component memory leak error

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,23 +1,31 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [loadingStatus, setLoadingStatus] = useState(true);
+  const isMounted = useRef(true);
 
   useEffect(() => {
-    const auth = getAuth();
+    if (isMounted) {
+      const auth = getAuth();
 
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoggedIn(true);
-      }
-      setLoadingStatus(false);
-    });
-  }, []);
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
+        }
+        setLoadingStatus(false);
+      });
+    }
+
+    return () => (isMounted.current = false);
+  }, [isMounted]);
 
   return { loggedIn, loadingStatus };
 };
 
 // custom hook using info from:
 // https://stackoverflow.com/questions/65505665/protected-route-with-firebase
+
+// react unmounted component memory leak fix from:
+// https://stackoverflow.com/questions/59780268/cleanup-memory-leaks-on-an-unmounted-component-in-react-hooks
