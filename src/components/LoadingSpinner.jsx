f7976be9aa37ea383051b2878296e7717aac426b
Ivan Olmo <ivan.olmo@gmail.com>
2022-01-23 11:02:50 -0600

    Add loading spinner reusable component

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoadingSpinner() {
+  return (
+    <div className='loading__container'>
+      <div className='loading__spinner'></div>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
