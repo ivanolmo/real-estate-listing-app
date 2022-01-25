e623113014a4a891bf682a0397f8a9e7d1f53419
Ivan Olmo <ivan.olmo@gmail.com>
2022-01-24 18:24:19 -0600

    Refactor Explore page with rent/sale category pages

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,5 +1,30 @@
+import { Link } from 'react-router-dom';
+
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg';
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg';
+
 function Explore() {
-  return <div className='page__header'>Explore</div>;
+  return (
+    <div className='explore'>
+      <header>
+        <p className='page__header'>Explore</p>
+      </header>
+      <main>
+        {/* image slider */}
+        <p className='explore__heading'>Categories</p>
+        <div className='explore__categories'>
+          <Link to='/category/rent'>
+            <img className='category__img' src={rentCategoryImage} alt='rent' />
+            <p className='category__name'>Places for rent</p>
+          </Link>
+          <Link to='/category/sell'>
+            <img className='category__img' src={sellCategoryImage} alt='sell' />
+            <p className='category__name'>Places for sale</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Explore;
