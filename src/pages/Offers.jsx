de074de6acadfa10453c770106eeba156f8a467b
Ivan Olmo <ivan.olmo@gmail.com>
2022-01-27 13:17:29 -0600

    Refactor with logic to retrieve listings with offers
    Will only return listings where the offer flag is set to true

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,5 +1,93 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore';
+import { toast } from 'react-toastify';
+
+import { db } from '../firebase.config';
+import ListingItem from '../components/ListingItem';
+import LoadingSpinner from '../components/LoadingSpinner';
+
 function Offers() {
-  return <div className='page__header'>Offers</div>;
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // create a reference to the listings collection
+        const listingsRef = collection(db, 'listings');
+
+        // create a query to the db, returning only those where offer is true
+        const q = query(
+          listingsRef,
+          where('offer', '==', true),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        );
+
+        // execute the query
+        const querySnap = await getDocs(q);
+
+        // initialize empty array that will contain listings data
+        let listings = [];
+
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        toast.error(
+          'There was an error querying the database, please try again...'
+        );
+      }
+    };
+
+    fetchListings();
+  }, []);
+
+  return (
+    <div className='category'>
+      <header>
+        <p className='page__header'>Offers</p>
+      </header>
+
+      {loading ? (
+        <LoadingSpinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className='category__listings'>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>There are currently no offers available.</p>
+      )}
+    </div>
+  );
 }
 
 export default Offers;
