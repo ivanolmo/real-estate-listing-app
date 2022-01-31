213f950f5e896e68fef3d6540032395f66427bf6
Ivan Olmo <ivan.olmo@gmail.com>
2022-01-30 21:01:03 -0600

    Add image upload function to helper file

---
+++ src/utils/uploadImage.js
@@ -0,0 +1,52 @@
+import { getAuth } from 'firebase/auth';
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage';
+import { v4 as uuidv4 } from 'uuid';
+
+// Uploads an image to Firebase storage
+const uploadImage = async (image) => {
+  const auth = getAuth();
+
+  return new Promise((resolve, reject) => {
+    const storage = getStorage();
+    const filename = `${auth.currentUser.id}-${image.name}-${uuidv4()}`;
+
+    const storageRef = ref(storage, `images/${filename}`);
+
+    const uploadTask = uploadBytesResumable(storageRef, image);
+
+    // copied from Firebase docs
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log(`Upload is ${progress}% done`);
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+          default:
+            break;
+        }
+      },
+      (error) => {
+        reject(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          resolve(downloadURL);
+        });
+      }
+    );
+  });
+};
+
+export default uploadImage;
