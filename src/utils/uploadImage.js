97963d417e55d8987d5274cda665e5b8781588f9
Ivan Olmo <ivan.olmo@gmail.com>
2022-02-01 10:56:08 -0600

    Refactor to remove logging upload progress to console

--- src/utils/uploadImage.js
+++ src/utils/uploadImage.js
@@ -23,15 +23,10 @@ const uploadImage = async (image) => {
     uploadTask.on(
       'state_changed',
       (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log(`Upload is ${progress}% done`);
         switch (snapshot.state) {
           case 'paused':
-            console.log('Upload is paused');
             break;
           case 'running':
-            console.log('Upload is running');
             break;
           default:
             break;
