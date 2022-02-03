6f6c67ac78adc57bc934c8f186565943f4db0f72
Ivan Olmo <ivan.olmo@gmail.com>
2022-02-03 12:19:13 -0600

    Add new component to Routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
 import Category from './pages/Category';
+import Listing from './pages/Listing';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import CreateListing from './pages/CreateListing';
@@ -20,6 +21,10 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/category/:categoryName' element={<Category />} />
+          <Route
+            path='/category/:categoryName/:listingId'
+            element={<Listing />}
+          />
           <Route path='/offers' element={<Offers />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
