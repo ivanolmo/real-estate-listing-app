6926d60e0948b84be089f7bf967481fc44270e57
Ivan Olmo <ivan.olmo@gmail.com>
2022-02-04 22:46:35 -0600

    Add new property owner Contact component
    Allows an app user to send a property owner an email
    Email contains the property name as the subject line
    Email also contains the user message in the email body

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from 'react';
+import { useParams, useSearchParams } from 'react-router-dom';
+import { doc, getDoc } from 'firebase/firestore';
+
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+
+function Contact() {
+  const [message, setMessage] = useState('');
+  const [owner, setOwner] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const params = useParams();
+
+  const onChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  useEffect(() => {
+    const getOwner = async () => {
+      const docRef = doc(db, 'users', params.ownerId);
+      const docSnapshot = await getDoc(docRef);
+
+      if (docSnapshot.exists()) {
+        setOwner(docSnapshot.data());
+      } else {
+        toast('Error getting owner data');
+      }
+    };
+    getOwner();
+  }, [params.ownerId]);
+
+  return (
+    <div className='page__container'>
+      <header>
+        <p className='page__header'>Contact Owner</p>
+      </header>
+
+      <main>
+        <div className='contact__owner'>
+          <p className='owner__name'>Contact {owner?.name}</p>
+        </div>
+
+        <form className='message__form'>
+          <div className='message__div'>
+            <label htmlFor='message' className='message__label'>
+              Message
+            </label>
+            <textarea
+              name='message'
+              id='message'
+              className='textarea'
+              value={message}
+              onChange={onChange}
+            ></textarea>
+          </div>
+          <a
+            href={`mailto:${owner?.email}?Subject=${searchParams.get(
+              'listingName'
+            )}&body=${message}`}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button type='button' className='primary__btn'>
+              Send Message
+            </button>
+          </a>
+        </form>
+      </main>
+    </div>
+  );
+}
+
+export default Contact;
