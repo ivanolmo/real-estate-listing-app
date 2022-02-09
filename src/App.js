cbf0148c2e5abf023595b44af9f5a879b8813424
Ivan Olmo <ivan.olmo@gmail.com>
2022-02-08 21:42:07 -0600

    Add EditListing page to routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Listing from './pages/Listing';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import CreateListing from './pages/CreateListing';
+import EditListing from './pages/EditListing';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
@@ -31,6 +32,7 @@ function App() {
             <Route path='/profile' element={<Profile />} />
           </Route>
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/edit-listing/:listingId' element={<EditListing />} />
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
